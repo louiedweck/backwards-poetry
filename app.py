9f7decc043c1a790a8370084fc36131e186138b1
louiedweck <dweck.louis@gmail.com>
2019-11-02 03:17:54 -0700

    poem is called from file

--- app.py
+++ app.py
@@ -1,30 +1,6 @@
 import random
 
-poem = '''Two roads diverged in a yellow wood,
-And sorry I could not travel both
-And be one traveler, long I stood
-And looked down one as far as I could
-To where it bent in the undergrowth;
-
-Then took the other, as just as fair,
-And having perhaps the better claim,
-Because it was grassy and wanted wear;
-Though as for that the passing there
-Had worn them really about the same,
-
-And both that morning equally lay
-In leaves no step had trodden black.
-Oh, I kept the first for another day!
-Yet knowing how way leads on to way,
-I doubted if I should ever come back.
-
-I shall be telling this with a sigh
-Somewhere ages and ages hence:
-Two roads diverged in a wood, and I—
-I took the one less traveled by,
-And that has made all the difference
-'''
-
+poem = open("poems/poem.txt", "r").read()
 lines = poem.split("\n")
 
 
@@ -50,7 +26,7 @@ def lines_printed_random(lines):
 
 
 def my_own_rearrange(lst):
-    ''' Function of your choice that rearranges the poem in a unique way'''
+    ''' Function that sorts poem by even and odd lines. Proceeds to add the even lines to one list and the odds to another. Finally prints even list and then odd list.'''
     evens = []
     odds = []
     for i in range(len(lst)):
